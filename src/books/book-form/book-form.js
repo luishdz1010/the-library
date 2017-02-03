bbafba82a813bba14e4aeaeecfeadbc285363748
luis.hernandez <luis.hernandez@adventa.mx>
2017-02-02 23:47:20 -0600

    Improves book form usability with date widget

--- src/books/book-form/book-form.js
+++ src/books/book-form/book-form.js
@@ -1,12 +1,13 @@
-import 'ui-select/dist/select.css';
 import './book-form.scss';
 import {module} from 'angular';
-import uiSelect from 'ui-select';
+import uiSelect from '../../vendor/ui-select';
+import uiDatePickerPopup from 'angular-ui-bootstrap/src/datepickerPopup';
 import booksService from '../books.service';
 import bookFormComponent from './book-form.component';
 
 export default module('library.books.form', [
   uiSelect,
+  uiDatePickerPopup,
   booksService
 ])
 
